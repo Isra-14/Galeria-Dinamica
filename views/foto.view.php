282184f22e5cd9b669386622aa093b54507ae584
Israel Sanchez <ishisra14@hotmail.com>
2022-02-03 18:51:46 -0600

    Estructura detalles y subir foto

---
+++ views/foto.view.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+    <!-- // ! FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <!-- // ! CSS -->
+    <link rel="stylesheet" href="css/estilos.css">
+
+    <title>Galeria</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="titulo">Foto: 1.jpg</h1>
+        </div>
+    </header>
+
+    <div class="contenedor">
+        <div class="foto">
+            <img src="img/1.jpg" alt="Imagen 1">
+            <p class="texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, accusamus.</p>
+            <a href="index.php" class="regresar"><i class="fa fa-long-arrow-left"></i> Regresar</a>
+        </div>
+    </div>
+
+    <footer>
+        <p class="copyright">
+            Galeria creada por Israel Sanchez 2022
+        </p>
+    </footer>
+
+
+</body>
+</html>
---
+++ views/subir.view.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+    <!-- // ! FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <!-- // ! CSS -->
+    <link rel="stylesheet" href="css/estilos.css">
+
+    <title>Galeria</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="titulo">Subir foto</h1>
+        </div>
+    </header>
+
+    <div class="contenedor">
+        <form class="formulario" method = "post"  enctype = "multipart/form-data" action="<?php echo htmlspecialchars( $_SERVER['PHP_SELF']); ?>">
+            <label for="foto">Selecciona tu foto</label>
+            <input type="file" name="foto" id="foto">
+            
+            <label for="titulo">Titulo de la foto</label>
+            <input type="text" name="titulo" id="titulo">
+            
+            <label for="texto">Descripcion</label>
+            <textarea name="texto" id="texto" placeholder="Ingresa unsa descripcion"></textarea>
+            
+            <input type="submit" class="submit" value="Subir foto">
+        </form>
+    </div>
+
+    <footer>
+        <p class="copyright">
+            Galeria creada por Israel Sanchez 2022
+        </p>
+    </footer>
+
+
+</body>
+</html>
